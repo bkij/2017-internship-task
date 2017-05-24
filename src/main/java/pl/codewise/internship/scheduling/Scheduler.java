06a6185d8ad7e5fff3850fd936f009e57d7a0463
B Kij <duelbart5@gmail.com>
2017-05-24 09:49:03 +0200

    Change scheduler interface to be more generic
    As in the example code in codewise repo

--- src/main/java/pl/codewise/internship/scheduling/Scheduler.java
+++ src/main/java/pl/codewise/internship/scheduling/Scheduler.java
@@ -2,9 +2,9 @@ package pl.codewise.internship.scheduling;
 
 public interface Scheduler {
     /*
-     * Timeout in seconds after running
+     * ExpirationTime in seconds after running
      * the start method
      */
-    public Long start(long timeout, Runnable callback);
-    public void stop(Long timerId);
+    public TimerId start(long expirationTime, Runnable callback);
+    public void stop(TimerId timerId);
 }
---
+++ src/main/java/pl/codewise/internship/scheduling/TimerId.java
@@ -0,0 +1,4 @@
+package pl.codewise.internship.scheduling;
+
+public interface TimerId {
+}
